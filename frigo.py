c5e3c19d8b20ffe21f02960701e62460a7daec0d
fenwick <t6heveli@enib.fr>
2019-12-15 13:16:52 +0100

    Mémoire tri/filtre/recherche

--- frigo.py
+++ frigo.py
@@ -1,5 +1,5 @@
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 from flask import Flask, Response, flash, request, redirect, url_for, send_from_directory
 from flask import redirect
 from flask import render_template
@@ -13,13 +13,131 @@ from flask_basicauth import BasicAuth
 from flask_admin.contrib import sqla
 from werkzeug.exceptions import HTTPException
 
+from pyzbar.pyzbar import decode
+from PIL import Image, ExifTags
+import openfoodfacts
+
+
+# fonction qui permet le tri et le filtrage des aliments.
+# 3 paramètres d'entrée, la liste
+def affiche(aliments, filter, order):
+    ajd = datetime.today()
+
+    # if filter == "sec":
+    #     aliments = aliments.query.filter_by(frais='sec')
+    # elif filter == "frais":
+    #     aliments = aliments.query.filter_by(frais='frais')
+    # elif filter == "ok":
+    #     aliments = aliments.query.filter(Aliment.peremption > ajd)
+    # elif filter == "perime":
+    #     aliments = aliments.filter(Aliment.peremption < ajd)
+    #
+    # if order == "date+":
+    #     aliments = aliments.query.order_by(desc(Aliment.id))
+    # elif order == "date-":
+    #     aliments = aliments.query.order_by(Aliment.id)
+    # elif order == "dlc+":
+    #     aliments = aliments.query.order_by(Aliment.peremption)
+    # elif order == "dlc-":
+    #     aliments = aliments.query.order_by(desc(Aliment.peremption))
+    # elif order == "name":
+    #     aliments = aliments.query.order_by(Aliment.titre)
+    # else:
+    #     aliments = aliments.query.order_by(desc(Aliment.ajout))
+
+
+    if filter == "none":
+        if order == "date+":
+            aliments = Aliment.query.order_by(desc(Aliment.id))
+        elif order == "date-":
+            aliments = Aliment.query.order_by(Aliment.id)
+        elif order == "dlc+":
+            aliments = Aliment.query.order_by(Aliment.peremption)
+        elif order == "dlc-":
+            aliments = Aliment.query.order_by(desc(Aliment.peremption))
+        elif order == "name":
+            aliments = Aliment.query.order_by(Aliment.titre)
+        else:
+            aliments = Aliment.query.order_by(desc(Aliment.ajout))
+    elif filter == "sec":
+        if order == "date+":
+            aliments = Aliment.query.order_by(Aliment.id).filter_by(frais='sec')
+        elif order == "date-":
+            aliments = Aliment.query.order_by(desc(Aliment.id)).filter_by(frais='sec')
+        elif order == "dlc+":
+            aliments = Aliment.query.order_by(Aliment.peremption).filter_by(frais='sec')
+        elif order == "dlc-":
+            aliments = Aliment.query.order_by(desc(Aliment.peremption)).filter_by(frais='sec')
+        elif order == "name":
+            aliments = Aliment.query.order_by(Aliment.titre).filter_by(frais='sec')
+        else:
+            aliments = Aliment.query.order_by(desc(Aliment.ajout)).filter_by(frais='sec')
+    elif filter == "frais":
+        if order == "date+":
+            aliments = Aliment.query.order_by(desc(Aliment.id)).filter_by(frais='frais')
+        elif order == "date-":
+            aliments = Aliment.query.order_by(Aliment.id).filter_by(frais='frais')
+        elif order == "dlc+":
+            aliments = Aliment.query.order_by(Aliment.peremption).filter_by(frais='frais')
+        elif order == "dlc-":
+            aliments = Aliment.query.order_by(desc(Aliment.peremption)).filter_by(frais='frais')
+        elif order == "name":
+            aliments = Aliment.query.order_by(Aliment.titre).filter_by(frais='frais')
+        else:
+            aliments = Aliment.query.order_by(desc(Aliment.ajout)).filter_by(frais='frais')
+
+    elif filter == "ok":
+        if order == "date+":
+            aliments = Aliment.query.order_by(desc(Aliment.id)).filter(Aliment.peremption > ajd)
+        elif order == "date-":
+            aliments = Aliment.query.order_by(Aliment.id).filter(Aliment.peremption > ajd)
+        elif order == "dlc+":
+            aliments = Aliment.query.order_by(Aliment.peremption).filter(Aliment.peremption > ajd)
+        elif order == "dlc-":
+            aliments = Aliment.query.order_by(desc(Aliment.peremption)).filter(Aliment.peremption > ajd)
+        elif order == "name":
+            aliments = Aliment.query.order_by(Aliment.titre).filter(Aliment.peremption > ajd)
+        else:
+            aliments = Aliment.query.order_by(desc(Aliment.ajout)).filter(Aliment.peremption > ajd)
+
+    elif filter == "perime":
+        if order == "date+":
+            aliments = Aliment.query.order_by(desc(Aliment.id)).filter(Aliment.peremption < ajd)
+        elif order == "date-":
+            aliments = Aliment.query.order_by(Aliment.id).filter(Aliment.peremption < ajd)
+        elif order == "dlc+":
+            aliments = Aliment.query.order_by(Aliment.peremption).filter(Aliment.peremption < ajd)
+        elif order == "dlc-":
+            aliments = Aliment.query.order_by(desc(Aliment.peremption)).filter(Aliment.peremption < ajd)
+        elif order == "name":
+            aliments = Aliment.query.order_by(Aliment.titre).filter(Aliment.peremption < ajd)
+        else:
+           aliments = Aliment.query.order_by(desc(Aliment.ajout)).filter(Aliment.peremption < ajd)
+
+    return aliments
+
+
+# fonction qui permet la rechercher (search bar)
+def find(txt, aliments):
+    result = []
+    for aliment in aliments:
+        titre = aliment.titre.lower()
+        desc = aliment.desc.lower()
+        frais = aliment.frais
+        result1 = titre.find(txt)
+        result2 = desc.find(txt)
+        result3 = frais.find(txt)
+        if result1 != -1 or result2 != -1 or result3 != -1:
+            result.append(aliment)
+    return (result)
+
+
 project_dir = os.path.dirname(os.path.abspath(__file__))
 database_file = "sqlite:///{}".format(os.path.join(project_dir, "database.db"))
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = database_file
 db: SQLAlchemy = SQLAlchemy(app)
-
 
 UPLOAD_FOLDER = './static/img_db/'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
@@ -29,10 +147,11 @@ class AuthException(HTTPException):
     def __init__(self, message):
         super().__init__(message, Response(
             message, 401,
-            {'WWW-Authenticate': 'Basic realm="Login Required"'}
+            {'WWW-Authenticate': 'Basic realm="Authentification"'}
         ))
 
 
+# Utilisation d'une librairie qui permet  l'authentification de l'administration
 class ModelView(sqla.ModelView):
     def is_accessible(self):
         if not basic_auth.authenticate():
@@ -44,6 +163,7 @@ class ModelView(sqla.ModelView):
         return redirect(basic_auth.challenge())
 
 
+# Model qui permet de définir notre table dans la base de donné.
 class Aliment(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     titre = db.Column(db.String(80))
@@ -55,6 +175,8 @@ class Aliment(db.Model):
     dlc = db.Column(db.String(30))
     nom = db.Column(db.String(50))
     image = db.Column(db.String(50))
+    nutriscore = db.Column(db.String(10))
+    cb = db.Column(db.String(30))
 
     def __repr__(self):
         return "<Title: {}>".format(self.titre), \
@@ -64,8 +186,8 @@ class Aliment(db.Model):
                "frais:{}".format(self.frais), \
                "desc:{}".format(self.desc), \
                "dlc:{}".format(self.dlc), \
-               "nom:{}".format(self.nom)
-               # "image:{}".format(self.image)
+               "nom:{}".format(self.nom), \
+               "id:{}".format(self.id)
 
 
 admin = Admin(app)
@@ -73,7 +195,6 @@ admin.add_view(ModelView(Aliment, db.session))
 path_static = op.join(op.dirname(__file__), 'static')
 path_db = op.join(op.dirname(__file__), 'img_db')
 admin.add_view(FileAdmin(path_static, '/static/', name='Static Files'))
-#admin.add_view(FileAdmin(path_db, '/img_db/', name='image database'))
 
 
 @app.route('/logout')
@@ -87,7 +208,9 @@ def home():
     if request.form:
         try:
 
-
+            code = ''
+            nutriscore = ''
+            img = ''
             if request.method == 'POST':
                 # check if the post request has the file part
                 if 'file' not in request.files:
@@ -99,12 +222,65 @@ def home():
                 # submit an empty part without filename
                 if file.filename == '':
                     flash('No selected file')
-                    img_name=request.form.get("frais")+".jpg"
+                    img = url_for('static', filename='img_db/' + request.form.get("frais") + ".jpg")
                 if file and allowed_file(file.filename):
                     file.save(os.path.join(UPLOAD_FOLDER, file.filename))
-                    img_name=file.filename
+                    img_name = file.filename
+                    try:
+                        codes = decode(Image.open('./static/img_db/' + img_name))
+                        print(codes)
+                        print(1)
+                        if len(codes) > 0:
+                            print(codes)
+                            code = codes[0].data.decode("utf-8")
+                        else:
+                            code = ''
+                        print(2)
+                        product = openfoodfacts.products.get_product(code)
+                        print(2.5)
+                        if product['status_verbose'] == 'product found':
+                            print(3)
+                            try:
+                                img = product['product']['image_small_url']
+                                os.remove("./static/img_db/" + img_name)
+                                print(4)
 
-            aliment = Aliment(titre=request.form.get("titre"),
+                            except:
+                                basewidth = 500
+
+                                file = Image.open('./static/img_db/' + img_name)
+                                try:
+                                    for orientation in ExifTags.TAGS.keys():
+                                        if ExifTags.TAGS[orientation] == 'Orientation':
+                                            break
+                                    exif = dict(file._getexif().items())
+
+                                    if exif[orientation] == 3:
+                                        file = file.rotate(180, expand=True)
+                                    elif exif[orientation] == 6:
+                                        file = file.rotate(270, expand=True)
+                                    elif exif[orientation] == 8:
+                                        file = file.rotate(90, expand=True)
+
+                                except (AttributeError, KeyError, IndexError):
+                                    # cases: image don't have getexif
+                                    pass
+                                wpercent = (basewidth / float(file.size[0]))
+                                hsize = int((float(file.size[1]) * float(wpercent)))
+                                file = file.resize((basewidth, hsize), Image.ANTIALIAS)
+                                file.save('./static/img_db/' + img_name)
+                                file.close()
+                                img = url_for('static', filename='img_db/' + img_name)
+
+                            try:
+                                nutriscore = product['product']['nutriscore_grade']
+                            except:
+                                nutriscore = '0'
+                    except:
+                        print('erreur')
+                        img = img_name
+
+            aliment = Aliment(titre=request.form.get("titre").capitalize(),
                               quantity=request.form.get("quantity"),
                               peremption=datetime.strptime(request.form.get("peremption"), '%Y-%m-%d'),
                               frais=request.form.get("frais"),
@@ -112,62 +288,38 @@ def home():
                               desc=request.form.get("desc"),
                               dlc=request.form.get("dlc"),
                               nom=request.form.get("nom"),
-                              image=img_name)
+                              image=img,
+                              nutriscore=nutriscore,
+                              cb=code
+                              )
 
             db.session.add(aliment)
             db.session.commit()
+            return redirect("/")
         except Exception as e:
             print("Failed to add aliment")
             print(e)
-
-
 
     aliments = Aliment.query.all()
     return render_template("add.html", aliments=aliments)
 
 
-@app.route("/update", methods=["POST"])
-def update():
-    try:
-        newtitle = request.form.get("newtitle")
-        oldtitle = request.form.get("oldtitle")
-        aliment = Aliment.query.filter_by(titre=oldtitle).first()
-        aliment.titre = newtitle
-        db.session.commit()
-    except Exception as e:
-        print("Couldn't update aliment titre")
-        print(e)
-    return redirect("/")
-
-
 @app.route('/', methods=["GET", "POST"])
 def list():
+    now = datetime.date(datetime.today())
+    twodays = datetime.date(datetime.today() + timedelta(days=2))
     filter = request.form.get("filter")
     order = request.form.get("order")
-    if filter == "sec":
-        aliments = Aliment.query.filter_by(frais='sec')
-    elif filter == "frais":
-        aliments = Aliment.query.filter_by(frais='frais')
-    elif filter == "ok":
-        ajd = datetime.today()
-        aliments = Aliment.query.filter(Aliment.peremption > ajd)
-    elif filter == "perime":
-        ajd = datetime.today()
-        aliments = Aliment.query.filter(Aliment.peremption < ajd)
-    elif order == "date+":
-        aliments = Aliment.query.order_by(Aliment.ajout)
-    elif order == "date-":
-        aliments = Aliment.query.order_by(desc(Aliment.ajout))
-    elif order == "dlc+":
-        aliments = Aliment.query.order_by(Aliment.peremption)
-    elif order == "dlc-":
-        aliments = Aliment.query.order_by(desc(Aliment.peremption))
-    elif order == "name":
-        aliments = Aliment.query.order_by(Aliment.titre)
-    else:
-        aliments = Aliment.query.order_by(desc(Aliment.ajout))
-
-    return render_template("index.html", aliments=aliments)
+    aliments = Aliment.query.all()
+    res = request.form.get("search")
+    if res != None :
+        aliments = find(res, aliments)
+    else :
+        filter = request.form.get("filter")
+        order = request.form.get("order")
+        aliments = affiche(aliments, filter, order)
+    return render_template("index.html", search=res, filter=filter, order=order, aliments=aliments, now=now,
+                           twodays=twodays)
 
 
 @app.route("/delete", methods=["POST"])
@@ -176,8 +328,11 @@ def delete():
     aliment = Aliment.query.filter_by(id=iddelete).first()
     db.session.delete(aliment)
     db.session.commit()
-    if not ((aliment.image=="frais.jpg") or (aliment.image=="sec.jpg")) :
-        os.remove("./static/img_db/"+aliment.image)
+    if not ((aliment.image == "/static/img_db/frais.jpg") or (aliment.image == "/static/img_db/sec.jpg")):
+        try:
+            os.remove("." + aliment.image)
+        except:
+            pass
     return redirect("/")
 
 
@@ -189,7 +344,7 @@ def prendre():
     aliment = Aliment.query.filter_by(id=idtake).first()
     aliment.quantity = int(oldqty) - int(take)
     db.session.commit()
-    if aliment.quantity == 0:
+    if aliment.quantity <= 0:
         db.session.delete(aliment)
         db.session.commit()
         return redirect("/")
@@ -212,14 +367,11 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-
-
-
 if __name__ == "__main__":
     app.secret_key = os.urandom(24)
     app.config['BASIC_AUTH_USERNAME'] = 'admin'
     app.config['BASIC_AUTH_PASSWORD'] = '123'
     basic_auth = BasicAuth(app)
     app.debug = True
-    app.run(host='192.168.0.11', port=5000)
-    #app.run()
+    # app.run(host='192.168.0.25', port=80)
+    app.run()
